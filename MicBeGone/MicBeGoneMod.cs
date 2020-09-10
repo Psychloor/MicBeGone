eb5f7cf0682685dbb840a0fd356ebf32092396e6
Kevin Blomqvist <blomqvistkevin@hotmail.com>
2020-09-10 22:34:05 +0200

    removed harmony patch

--- MicBeGone/MicBeGoneMod.cs
+++ MicBeGone/MicBeGoneMod.cs
@@ -16,6 +16,10 @@ namespace MicBeGone
 
     using MelonLoader;
 
+    using UnityEngine;
+
+    using VRC.Core;
+
     public sealed class MicBeGoneMod : MelonMod
     {
 
@@ -23,28 +27,33 @@ namespace MicBeGone
 
         private static bool micEnabled;
 
+        private HudVoiceIndicator hudVoiceIndicator;
+
         public override void OnApplicationStart()
         {
             MelonPrefs.RegisterCategory(SettingsCategory, "Mic Be-Gone");
             MelonPrefs.RegisterBool(SettingsCategory, "MicEnabled", micEnabled, "Mic Enabled");
             micEnabled = MelonPrefs.GetBool(SettingsCategory, "MicEnabled");
+        }
 
-            harmonyInstance.Patch(
-                typeof(HudVoiceIndicator).GetMethod(nameof(HudVoiceIndicator.Update), BindingFlags.Public | BindingFlags.Instance),
-                null,
-                new HarmonyMethod(typeof(MicBeGoneMod).GetMethod(nameof(MicPatch), BindingFlags.NonPublic | BindingFlags.Static)));
+        public override void VRChat_OnUiManagerInit()
+        {
+            hudVoiceIndicator = Object.FindObjectOfType<HudVoiceIndicator>();
+            ApplySettings();
+        }
+
+        private void ApplySettings()
+        {
+            hudVoiceIndicator.enabled = micEnabled;
+            if (micEnabled) return;
+            hudVoiceIndicator.field_Private_Image_0.enabled = false;
+            hudVoiceIndicator.field_Private_Image_1.enabled = false;
         }
 
         public override void OnModSettingsApplied()
         {
             micEnabled = MelonPrefs.GetBool(SettingsCategory, "MicEnabled");
-        }
-
-        private static void MicPatch(ref HudVoiceIndicator __instance)
-        {
-            if (micEnabled) return;
-            __instance.field_Private_Image_0.enabled = false;
-            __instance.field_Private_Image_1.enabled = false;
+            ApplySettings();
         }
 
     }
